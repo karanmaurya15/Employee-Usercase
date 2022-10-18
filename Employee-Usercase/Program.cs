5686a795646b5ec374dcec783ee7eefe4eef6da1
karanmaurya15 <karanmaurya9663@gmail.com>
2022-10-18 23:38:38 +0530

    UC8- Emplyee Wage for Multiple Companies

--- Employee-Usercase/Program.cs
+++ Employee-Usercase/Program.cs
@@ -4,19 +4,17 @@
     {
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
-        public const int EMP_RATE_PER_HOUR = 100;
-        public const int NUM_OF_WORKING_DAYS = 20;
-        public const int MAX_HRS_IN_MONTH = 100;
 
-        public static int computeEmpWage()
+
+
+        public static int computeEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
         {
             int empHrs = 0;
             int totalEmpHrs = 0;
             int totalWorkingDays = 0;
 
-            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+            while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
             {
-
                 totalWorkingDays++;
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
@@ -32,17 +30,19 @@
                         empHrs = 0;
                         break;
                 }
+
                 totalEmpHrs += empHrs;
                 Console.WriteLine("Day#:" + totalWorkingDays + "Emp Hrs : " + empHrs);
 
             }
-            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Totak Emp Wage : " + totalEmpWage);
+            int totalEmpWage = totalEmpHrs * empRatePerHour;
+            Console.WriteLine("Total Emp Wage for company : " + company + " is : " + totalEmpWage);
             return totalEmpWage;
         }
         static void Main(string[] args)
         {
-            computeEmpWage();
+            computeEmpWage("Amazon", 20, 20, 100);
+            computeEmpWage("Tata", 15, 20, 50);
             
         }
     }
