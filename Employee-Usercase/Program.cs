50b5c2e32d4a4906a824614f922d013d724b49de
karanmaurya15 <karanmaurya9663@gmail.com>
2022-10-18 22:15:41 +0530

    UC-2 Employee Wages

--- Employee-Usercase/Program.cs
+++ Employee-Usercase/Program.cs
@@ -5,18 +5,24 @@
         static void Main(string[] args)
         {
             int IS_FULL_TIME = 1;
-
+            int EMP_RATE_PER_HOUR = 200;
+            //Variables
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
-
+            //Computation
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee Wage is :" + empWage);
+            Console.ReadLine();
         }
     }
 }
