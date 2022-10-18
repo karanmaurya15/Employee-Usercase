98530d63a0a634cd99ac54f942a88fc31fd1796a
karanmaurya15 <karanmaurya9663@gmail.com>
2022-10-18 23:19:58 +0530

    UC7-Employee Wage using Classmethod

--- Employee-Usercase/Program.cs
+++ Employee-Usercase/Program.cs
@@ -2,15 +2,13 @@
 {
     internal class Program
     {
-
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
-        public const int EMP_RATE_PER_HOUR = 50;
+        public const int EMP_RATE_PER_HOUR = 100;
         public const int NUM_OF_WORKING_DAYS = 20;
         public const int MAX_HRS_IN_MONTH = 100;
 
-        static void Main(string[] args)
-
+        public static int computeEmpWage()
         {
             int empHrs = 0;
             int totalEmpHrs = 0;
@@ -18,6 +16,7 @@
 
             while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
             {
+
                 totalWorkingDays++;
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
@@ -33,14 +32,18 @@
                         empHrs = 0;
                         break;
                 }
-
                 totalEmpHrs += empHrs;
                 Console.WriteLine("Day#:" + totalWorkingDays + "Emp Hrs : " + empHrs);
 
             }
             int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Totak Emp Wage : " + totalEmpWage);
-           
+            return totalEmpWage;
+        }
+        static void Main(string[] args)
+        {
+            computeEmpWage();
+            
         }
     }
 }
