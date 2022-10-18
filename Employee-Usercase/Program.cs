5426f5406b4ff48110ca0522276411741932263f
karanmaurya15 <karanmaurya9663@gmail.com>
2022-10-18 22:40:59 +0530

    UC-4 Employee  Wage using switch case

--- Employee-Usercase/Program.cs
+++ Employee-Usercase/Program.cs
@@ -2,33 +2,35 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            int IS_FULL_TIME = 1;
-            int IS_PART_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
 
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 200;
+
+        static void Main(string[] args)
+
+        {
             int empHrs = 0;
             int empWage = 0;
 
             Random random = new Random();
 
             int empCheck = random.Next(0, 3);
-            if (empCheck == IS_FULL_TIME)
+            switch (empCheck)
             {
-                empHrs = 8;
-            }
-            else if (empCheck == IS_PART_TIME)
-            {
-                empHrs = 4;
-            }
-            else
-            {
-                empHrs = 0;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Emp Wage : " + empWage);
-
+           
         }
     }
 }
