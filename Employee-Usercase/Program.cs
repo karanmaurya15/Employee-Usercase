f2e37ab35f14bb5e4422c9fd8648446c16d47778
karanmaurya15 <karanmaurya9663@gmail.com>
2022-10-19 06:11:23 +0530

    UC9-Employee TotalWage by Company

--- Employee-Usercase/Program.cs
+++ Employee-Usercase/Program.cs
@@ -2,48 +2,14 @@
 {
     internal class Program
     {
-        public const int IS_FULL_TIME = 1;
-        public const int IS_PART_TIME = 2;
-
-
-
-        public static int computeEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
-        {
-            int empHrs = 0;
-            int totalEmpHrs = 0;
-            int totalWorkingDays = 0;
-
-            while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
-            {
-                totalWorkingDays++;
-                Random random = new Random();
-                int empCheck = random.Next(0, 3);
-                switch (empCheck)
-                {
-                    case IS_FULL_TIME:
-                        empHrs = 8;
-                        break;
-                    case IS_PART_TIME:
-                        empHrs = 4;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
-                }
-
-                totalEmpHrs += empHrs;
-                Console.WriteLine("Day#:" + totalWorkingDays + "Emp Hrs : " + empHrs);
-
-            }
-            int totalEmpWage = totalEmpHrs * empRatePerHour;
-            Console.WriteLine("Total Emp Wage for company : " + company + " is : " + totalEmpWage);
-            return totalEmpWage;
-        }
         static void Main(string[] args)
         {
-            computeEmpWage("Amazon", 20, 20, 100);
-            computeEmpWage("Tata", 15, 20, 50);
-            
+            EmpWageBuilderObject dMart = new EmpWageBuilderObject("Amazon", 20, 20, 100);
+            EmpWageBuilderObject jio = new EmpWageBuilderObject("Tata", 15, 20, 50);
+            dMart.computeEmpWage();
+            Console.WriteLine(dMart.toString());
+            jio.computeEmpWage();
+            Console.WriteLine(jio.toString());
         }
     }
 }
